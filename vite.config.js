a180d325859bd3f293cb131878164a7eedb04b28
maris668-cmyk <marislaila668@gmail.com>
2026-08-30 21:27:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/pangasinan-heritage-digital-showcase/',
   plugins: [vue()],
-  server: { host: 'localhost', port: 5173 }
+  server: {
+    host: 'localhost',
+    port: 5173
+  }
 })
